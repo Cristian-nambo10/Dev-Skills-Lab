3530f7e5119d049e6775c1c61f2ea5fedaace9d2
Cristian Nambo <nambocristian10@gmail.com>
2022-05-05 20:15:45 -0500

    Work in progress: did some javascript and styled more elements

--- css/js/script.js
+++ css/js/script.js
@@ -1,1 +1,10 @@
-// alert('Im here')
+let btn = document.getElementById('button');
+
+btn.addEventListener('click', function() {
+    let list = document.createElement('li')
+    let input = document.getElementById('text-box')
+    list.textContent = input.value
+    document.querySelector('ul').appendChild(list);
+    input.value = ''
+    console.log(list)
+})
